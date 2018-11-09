4a223125d9620c7be2b2feda0e4699cf6872df86
Craig Oda <craig@oppkey.com>
2018-11-09 05:49:35 -0800

    completed lesson #2

--- lessons/02/js/profile.js
+++ lessons/02/js/profile.js
@@ -17,7 +17,18 @@ d3.csv('data/bts-profiles.csv').then((data) => {
 function showData(memberName, data) {
   profileListing.selectAll('text').remove();
  data.forEach(element => {
-   console.log(element.name);
+   if (element.name == memberName) {
+     let yPosition = 50;
+     for (var memberInfo in element) {
+       let memberValue = element[memberInfo];
+       profileListing
+        .append('text')
+        .attr('x', 50)
+        .attr('y', yPosition)
+        .text(`${memberInfo}: ${memberValue}`);
+      yPosition += 50;
+     }
+   }
  });
 
 }
